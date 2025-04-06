652f89bd3282ec32cbed8857a451ec2bd7b1b686
Gabriel Pivoto <gabrielspivoto@gmail.com>
2025-04-05 22:04:00 -0300

    adding first exercise

---
+++ easy/141_linked_list_cycle/141_linked_list_cycle.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) return true;
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    
+    ListNode* node1 = new ListNode(3);
+    ListNode* node2 = new ListNode(2);
+    ListNode* node3 = new ListNode(0);
+    ListNode* node4 = new ListNode(-4);
+
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node2;
+
+    Solution s;
+    bool result = s.hasCycle(node1);
+
+    cout << (result ? "True" : "false") << endl;
+
+    return 0;
+}
